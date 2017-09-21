8e7e3e524364d93b0f4d36460d2908e8a0c3bace
w3spot <psyco.dev007@gmail.com>
2017-09-21 16:54:23 +0530

    first commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head><title>Install FFmpeg at CentOS6 with php5.4</title></head>
+<style>
+*{margin:0px;padding:0px;border:0px;font-family:sans-serif;}
+.code{font-family:courier;color:#999;border:1px solid #555;padding:10px;margin:10px;}
+</style>
+<body>
+<h1>FFMPEG Auto Installer By YesKullu</h1>
+<p>To install ffmpeg at your server please run below command..</p>
+
+<br /><br />
+<p class="code">
+wget http://apps.yeskullu.com/library/ffmpeg/ffmpeg.yeskullu<br />
+sh ./ffmpeg.yeskullu
+</p>
+<br /><br />
+Enjoy !!!
+</body>
+</html>
